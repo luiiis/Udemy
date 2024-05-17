ea191504bf671042328298c9bcb867c228688808
luis <lgarciiaruiiz@gmail.com>
2024-05-17 17:38:39 -0600

    video 9 tipos de datos

---
+++ JavaScript_DesdeCeroconNodeJS/01_TiposDeDatos.js
@@ -0,0 +1,45 @@
+//comentario de una sola linea
+
+/*
+comentario de multiples lineas
+*/
+
+/**
+ * Tipos de datos
+ */
+//numero -number
+console.log(27);
+console.log(27.56);
+
+//Texto -String
+console.log("Luis Garcia");
+console.log("Jesus Omar");
+console.log(`Hola , soy gabriel`);
+
+//valores logicos-boolean
+console.log(true);
+console.log(false);
+
+//indefinido -undefined
+console.log(undefined);
+
+//null
+console.log(null);
+
+/**
+ * Tipos de datos estructurales
+ */
+
+//Objeto --object
+console.log({
+  nombre: "Luis",
+  edad: 27,
+  fechaDeNacimiento: null,
+  esAlto: false,
+  ciudades: ["Quito", "Buenos Aires", "Nueva York"],
+  direccion: { callePrincipal: "Siempre viva" },
+});
+
+//Arreglo --array
+console.log(["Mexico", "Venezuela", "Ecuador"]);
+console.log([21, true, { nombre: "gabriel" }, [1, 2, 3, 4, 5]]);

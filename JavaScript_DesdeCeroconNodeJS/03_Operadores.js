39f46f751bf44d1bedad598d23c24ae11e718785
luis <lgarciiaruiiz@gmail.com>
2024-05-17 19:18:48 -0600

    =11.- Operadores de asignacion

---
+++ JavaScript_DesdeCeroconNodeJS/03_Operadores.js
@@ -0,0 +1,44 @@
+/**
+ * Operadores
+ */
+
+//Operadores de asignacion
+ var x = 2;
+ var y = true;
+
+ //operador de asingacion de adicion (+=)
+ var x = 2;
+ var y = 1;
+
+ x+=y;//x=x+y
+ console.log(x);
+
+ //operador de asingacion de resta (-=)
+ var x = 1;
+ var y = 2;
+ x-=y;//x=x-y
+ console.log(x);
+
+ //operador de asignacion de multiplicacion (*=)
+ var x = 1;
+ var y = 2;
+ x*=y;//x=x*y
+ console.log(x);
+
+ //operador de asignacion de divicion (/=)
+ var x = 1;
+ var y = 2;
+ x/=y;//x=x/y
+ console.log(x);
+
+ //operador de asignacion de modulo o reciduo (%=)
+ var x = 1;
+ var y = 2;
+ x%=y;//x=x%y
+ console.log(x);
+
+ //operador de asignacion de exponenciccion (**=)
+ var x = 2;
+ var y = 2;
+ x**=y;//x=x**y
+ console.log(x);

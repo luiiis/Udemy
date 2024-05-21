08491ee647b1c3cba5828bbae79e43195c67367e
luis <lgarciiaruiiz@gmail.com>
2024-05-21 15:53:32 -0600

    13 operadores aritmeticos

---
+++ JavaScript_DesdeCeroconNodeJS/05_OperadoresAritmeticos.js
@@ -0,0 +1,23 @@
+/**
+ * Operadores aritmeticos
+ * +,-,*,/,%,**
+ */
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+//Operadores de incremento (++)
+var numero =0;
+console.log(++numero);//1
+console.log(numero++);//1 se incrementa despues de asignarse
+console.log(numero);//2
+
+
+//operador de decremento(--)
+var numero =3;
+console.log(--numero);
+console.log(numero--);
+console.log(numero);

ad700247da75bea9fff44319d5dcb63211206e07
luis <lgarciiaruiiz@gmail.com>
2024-05-21 15:59:35 -0600

    14 operadores logicos

---
+++ JavaScript_DesdeCeroconNodeJS/06_OperadoresLogicos.js
@@ -0,0 +1,15 @@
+/*
+Operadores logicos
+ */
+
+//AND
+console.log(true&&true);
+console.log(2 > 3 && 1 <= 2);//false
+
+//OR
+console.log(true||true);//true
+console.log(flase||false);//false
+
+
+//NOT
+console.log(!true);//flase

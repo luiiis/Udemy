09619620b4bce8091d2d855410b0f19749b1cf72
luis <lgarciiaruiiz@gmail.com>
2024-05-27 19:32:11 -0600

    22 Ciclo for in y for of

--- JavaScript_DesdeCeroconNodeJS/11_CicloFor.js
+++ JavaScript_DesdeCeroconNodeJS/11_CicloFor.js
@@ -7,3 +7,32 @@
 for(let contador = 0; contador<=5;contador++){
     console.log('Hola mundo')
 }
+
+/**
+ * For in
+ * 
+ * for(variable operadorIN objeto){
+//instrucciones
+}
+ */
+var persona ={
+    nombre:'Luis',
+    apellido:'Garcia',
+    edad:23
+}
+for(let clave in persona){
+console.log(clave,persona[clave]);
+}
+/**IMPRIME
+ * nombre Luis
+apellido Garcia
+edad 23
+ */
+
+
+//for of
+var arreglo = [1,2,3,4,5];
+var nombe ='luis';
+for(let valor of nombe){
+console.log(valor);
+}

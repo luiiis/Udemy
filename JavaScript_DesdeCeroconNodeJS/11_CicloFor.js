4d8ad9208df06a49ec62b32fca16029e245cbf0d
luis <lgarciiaruiiz@gmail.com>
2024-05-27 19:20:23 -0600

    21 Ciclo for

---
+++ JavaScript_DesdeCeroconNodeJS/11_CicloFor.js
@@ -0,0 +1,9 @@
+/** 
+ * Ciclo for
+ * for(expresioninicial; expresionCondiciona; expresionDeActualizacion){
+    //instrucciones
+}
+*/
+for(let contador = 0; contador<=5;contador++){
+    console.log('Hola mundo')
+}
